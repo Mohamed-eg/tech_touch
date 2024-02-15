ef8d4cc649cac9c7ac9a86911f3eba179b336665
Mohamed-eg <mhmdalshayb39@gmail.com>
2024-02-15 08:11:02 +0200

    finsh

---
+++ src/app/Language/[uid]/page.jsx
@@ -0,0 +1,25 @@
+import UserInfoNav from "../../../../components/user-info/UserInfoNav";
+import UserInfoAddress from "../../../../components/user-info/UserInfoAddress";
+import UserInfoLayout from "../../../../components/user-info/UserInfoLayout";
+import MainHeader from "../../../../components/main-header";
+import FooterComp from "../../../../components/footer";
+
+const page = () => {
+  return (
+    <>
+    <MainHeader />
+    <UserInfoLayout className="mt-[200px]">
+      <UserInfoNav />
+      <h1>Languages</h1>
+    </UserInfoLayout>
+    <FooterComp
+        frameDivPosition="unset"
+        frameDivBottom="unset"
+        frameDivLeft="unset"
+        iconsCurvedLocationObjectFit="unset"
+      />
+    </>
+  );
+};
+
+export default page;
