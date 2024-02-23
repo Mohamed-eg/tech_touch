c0f582a28cfa675f4594d02f1e4aa0ee7b59ddbe
Mohamed-eg <mhmdalshayb39@gmail.com>
2024-02-23 06:16:56 +0200

    noProfile

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from "react-redux";
-import { faBars, faHome, faShop, faStream, faCog, faPerson } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHome, faShop, faStream, faCog, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import Link from 'next/link';
 
@@ -23,7 +23,8 @@ const NavItem: React.FC<NavItemProps> = ({ text, icon, href }) => {
 };
 
 const Sidebar: React.FC = () => {
-
+  const uid = useSelector((state: any) => state.categories.currentUser)
+  const List = useSelector((state: any) => state.wishList.List)
   const cart = useSelector((state: any) => state.products.cart)
 
   // const getTotalQuantity = () => {
@@ -47,13 +48,17 @@ const Sidebar: React.FC = () => {
       <div className={`fixed z-50 right-0 top-0 h-full w-64 overflow-y-auto bg-white shadow-md transition-all duration-300 transform ${sidebarOpen ? 'translate-x-0' : 'translate-x-full'}`}>
         <button className="absolute right-0 ml-6 mt-6 text-xl font-bold text-black" onClick={toggleSidebar}>&times;</button>
         <nav className="mt-10">
+          {uid ? <span className=" absolute flex w-3 h-3 bg-[#00cc00] rounded-full top-[80px] right-[210px]"></span> : <span className=" absolute flex w-3 h-3 bg-[#a7a7a7] rounded-full top-[-8px] "></span>}
+
+          {cart?.length === 0 ? null : <span className="bg-[#d61414] absolute top-[140px] right-[210px] px-1 text-white rounded-full ">{cart.length || 0}</span>}
+
+          {List?.length ? <span className="bg-[#d61414] absolute top-[180px] right-[210px] px-1 text-sm text-white rounded-full ">{List.length || 0}</span> : null}
           <NavItem text="Home" icon={faHome} href="/" />
-          <NavItem text="profile" icon={faPerson} href="/login" />
+          <NavItem text="profile" icon={faUser} href="/login" />
           <NavItem text="Categories" icon={faStream} href="/categories" />
           <NavItem text="Cart" icon={faShop} href="/card" />
           <NavItem text="wish list" icon={faHeart} href="/wishList" />
           <NavItem text="About" icon={faCog} href="/about" />
-          <span className="bg-[#d61414] absolute top-[140px] right-[210px] px-1 text-white rounded-full ">0</span>
         </nav>
       </div>
     </div>
